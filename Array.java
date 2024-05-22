3027e2a5b362244eb5dcd10e2218aa66e344a1cd
Qashu02 <hafizqasim5566@gmail.com>
2024-05-22 10:13:23 -0700

    Reverse of an array advance

--- Array.java
+++ Array.java
@@ -5,8 +5,19 @@ public class Array {
     public static void main(String[] args) {
 
         int[] arr = { 1, 24, 45, 2, 1, 3 };
-        for (int i = arr.length - 1; i >= 0; i--) {
+        int start, end;
+        start = 0;
+        end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(" " + arr[i]);
         }
+
     }
 }
