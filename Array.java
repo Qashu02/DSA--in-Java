434ee7d73ebf7591bba33990c66674b9ddfd46ae
Qashu02 <hafizqasim5566@gmail.com>
2024-06-02 21:11:17 -0700

    Array max value

--- Array.java
+++ Array.java
@@ -1,15 +1,15 @@
-// Array implementation fr minvalue 
+// Array implementation for max value 
 class Array {
     public static void main(String[] args) {
-        int[] arr = { 2, 10, 32, 12, 1 };
-        int min = arr[0];
+        int[] arr = { 2, 10, 32, 12, 1, 65, 102 };
+        int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
 
-            if (arr[i] < min) {
-                min = arr[i];
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
-        System.out.println(min);
+        System.out.println(max);
     }
 
 }
