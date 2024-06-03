ec8c8429a6df75f3872fcbed739e1c142f6db797
Qashu02 <hafizqasim5566@gmail.com>
2024-06-03 07:56:47 -0700

    Sending zero elements to end

--- Array.java
+++ Array.java
@@ -1,15 +1,32 @@
-// Array implementation for max value 
-class Array {
+// sending 0's to end of an Array
+/**
+ * Array
+ */
+public class Array {
+
     public static void main(String[] args) {
-        int[] arr = { 2, 10, 32, 12, 1, 65, 102 };
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
+        int[] arr = { 1, 0, 34, 0, 10, 7, 1, 5, 0, 23 };
+        int j = 0;
 
-            if (arr[i] > max) {
-                max = arr[i];
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] != 0 && arr[j] == 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
             }
-        }
-        System.out.println(max);
-    }
 
+            if (arr[j] != 0) {
+                j++;
+            }
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("\n" + arr[i]);
+
+        }
+
+    }
 }
